512f9276bac02330378c8972dfa97432583ebd8e
N. Othmane <naamane.othmane@gmail.com>
2013-08-31 09:53:08 +0100

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
 
-  get "user/new"
+  get "users/new"
+
+  get "users/new"
 
   root to: 'static_pages#home'
 
-  match '/signup', to: 'user#new'
+  match '/signup', to: 'users#new'
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
